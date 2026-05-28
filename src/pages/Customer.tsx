bb59034ae1011b204a7d81c329e755f47422a2e1
Piyumalsha Isurandi <piyumalshaisurandi@gmail.com>
2026-05-28 12:35:47 +0530

    second commit

--- src/pages/Customer.tsx
+++ src/pages/Customer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import  { useEffect, useState } from 'react'
 import { getAllCustomer, saveCustomer } from '../service/customer'
 
 const Customer = () => {
